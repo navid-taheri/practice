d67f112e3454659307f93ad280b778a05125e218
Navid Taheri <navid.taheri@meltwater.com>
2016-07-12 09:47:12 +0200

    add java-docker

---
+++ docker/java-docker/src/main/java/org/navid/App.java
@@ -0,0 +1,14 @@
+package org.navid;
+
+/**
+ * Created by navid on 2016-07-12.
+ */
+public class App {
+    public static void main(String[] args) throws Exception {
+        while ( true) {
+            System.out.print(".");
+            Thread.sleep(1000);
+        }
+
+    }
+}
